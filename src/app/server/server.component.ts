5890e02e3bfb667807a48982a2ea83d3bf6658c8
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-06-17 21:15:00 +0530

    String Interpolation in action through property and method

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
+    serverId: number = 1; // These variables are called 'properties'
+    serverStatus: string = "offline";
 
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
