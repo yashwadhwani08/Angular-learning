ea0a8d287189e005b4dcc47bc2d66b6cd2e3831d
yashwadhwani08 <yashwadhwani08@gmail.com>
2023-06-19 20:40:23 +0530

    Property Binding: native DOM HTML property 'disabled' being bound to TypeScript property 'allowNewServer'

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers', // name of a cutsom html element
   // selector: '[app-servers]', // attribute
   // selector: '.app-servers', //class
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // template: '<app-server></app-server><app-server></app-server>',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  // template: ` <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+
+  // A constructor is a method executed at the point of time, this component is created by angular
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void { }
+}
